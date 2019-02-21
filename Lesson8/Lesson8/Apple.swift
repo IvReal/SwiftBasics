e11490128532797acc3c3baf346274881afa044d
IvReal <ivreal@yandex.ru>
2019-02-22 00:28:48 +0300

    Initial game code

--- Lesson8/Lesson8/Apple.swift
+++ Lesson8/Lesson8/Apple.swift
@@ -1,9 +1,22 @@
-//
-//  File.swift
-//  Lesson8
-//
-//  Created by Iv on 22/02/2019.
-//  Copyright © 2019 Iv. All rights reserved.
-//
-
-import Foundation
+import UIKit
+import SpriteKit
+// Яблоко
+class Apple: SKShapeNode {
+    //определяем, как оно будет отрисовываться
+    convenience init(position: CGPoint) {
+        self.init()
+        // рисуем круг
+        path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 10, height: 10)).cgPath
+        // заливаем красным
+        fillColor = UIColor.red
+        // рамка тоже красная
+        strokeColor = UIColor.red
+        // ширина рамки 5 поинтов
+        lineWidth = 5
+        self.position = position
+        // Добавляем физическое тело, совпадающее с изображением яблока
+        self.physicsBody = SKPhysicsBody(circleOfRadius: 10.0, center:CGPoint(x:5, y:5))
+        // Категория - яблоко
+        self.physicsBody?.categoryBitMask = CollisionCategories.Apple
+    }
+}
--- Lesson8/Lesson8/Snake/SnakeHead.swift
+++ Lesson8/Lesson8/Snake/SnakeHead.swift
@@ -1,9 +1,13 @@
-//
-//  SnakeHead.swift
-//  Lesson8
-//
-//  Created by Iv on 22/02/2019.
-//  Copyright © 2019 Iv. All rights reserved.
-//
-
-import Foundation
+import UIKit
+class SnakeHead: SnakeBodyPart {
+    override init(atPoint point: CGPoint){
+        super.init(atPoint:point)
+        // категория - голова
+        self.physicsBody?.categoryBitMask = CollisionCategories.SnakeHead
+        // пересекается с телом, яблоком и границей экрана
+        self.physicsBody?.contactTestBitMask = CollisionCategories.EdgeBody | CollisionCategories.Apple | CollisionCategories.Snake
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
